0ab296dd69cbc85a7d1b1a6dfd54cc5c1fa2c288
victor schmoegel <victorschmoegel@gmail.com>
2024-11-11 19:25:36 -0300

    arrumando varchar que estava errado

--- init.sql
+++ init.sql
@@ -5,7 +5,7 @@ CREATE TABLE user (
     id INT AUTO_INCREMENT PRIMARY KEY,
     firstName VARCHAR(100) NOT NULL,
     lastName VARCHAR(100) NOT NULL,
-    email VARCHAR(1100) NOT NULL
+    email VARCHAR(100) NOT NULL
 );
 
 --TODO Crie a tabela de posts;
